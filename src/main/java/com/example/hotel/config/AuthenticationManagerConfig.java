fd51850b98f5a86e6416c55674eee204b731d337
RivenVSS <cerabn@mail.ru>
2021-06-21 15:43:06 +1000

    fix cors

--- src/main/java/com/example/hotel/config/AuthenticationManagerConfig.java
+++ src/main/java/com/example/hotel/config/AuthenticationManagerConfig.java
@@ -3,8 +3,10 @@ package com.example.hotel.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -18,7 +20,13 @@ class AuthenticationManagerConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/oauth/token");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().authorizeRequests().antMatchers("/**").permitAll();
+        http.cors().and().authorizeRequests()
+                .antMatchers("/login", "/security/**","/oauth/token").permitAll();
     }
 }
